7a9865b0d48dd219b3713c8d2d05336853a41eca
jisub yoon <yjisub11@gmail.com>
2017-07-03 11:31:09 +0900

    tranning express

--- server_sideJS/app.js
+++ server_sideJS/app.js
@@ -1,13 +1,26 @@
 var express=require('express');
 var app=express();
+
 app.use(express.static('public'));
+
 
 app.get('/',function (req,res){
     res.send('Hello World!!');
 });
+
 app.get('/login',(req,res)=>{
     res.send('Login Page!!');
 });
+app.get('/dynamic',(req,res)=>{
+    var output=`<body><h1>node!!</h1></body>`;
+    
+    res.send(output);
+});
+
+
+app.get('/route', (req,res)=>{
+    res.send('Route Hello <img src="/Capture001.png">');
+})
 app.listen(3000, function(){
     console.log('Example app listening in ',30000);
 });
